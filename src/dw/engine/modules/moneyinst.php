713231d3eaddf9b9c4f8f3fe1c19336d1bec828f
dench0 <dench0@yandex.ru>
2014-12-21 00:47:57 +0200

    dle global variables bug

--- src/dw/engine/modules/moneyinst.php
+++ src/dw/engine/modules/moneyinst.php
@@ -3,8 +3,8 @@
 define('moneyinst_conffile', dirname(__FILE__) . '/../data/moneyinstconfig.php');
 
 include_once(dirname(__FILE__) . '/../classes/moneyinst.class.php');
-
+global $_moneyinst;
 $_moneyinst = new MoneyInst();
 $_moneyinst->loadConfig();
-
+global $js_array;
 $js_array[] = "engine/classes/moneyinst/miobfs.js";
